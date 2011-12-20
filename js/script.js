ca9bd3c3f24d8f7566ec9301ec8b449589681eb5
Jim V <jfv@stowers.org>
2011-12-20 09:11:28 -0600

    working bioreports with sample report and lots of css and js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("#toc").tableOfContents(
+    $("#main"),
+    {
+      startLevel: 2,
+      depth: 3
+    });
+    
+  $('.big_table').tableFilter();
+  
+  $('.data_table').dataTable( {
+    "bPaginate": false,
+    "bSort": true
+  });
+  $('.basic_table').dataTable( {
+    "bPaginate": false,
+    "bLengthChange": false,
+    "bFilter": false,
+    "bSort": true,
+    "bInfo": false
+  });
+  $('#main h2').append("  <span class='header_accent'><a href='#' class='toggle'>hide</a></span>");
+  $('.section_toggle').append("  <span class='header_accent'><a href='#' class='toggle'>show</a></span>");
+  $('a.toggle').click(function() 
+    {
+      var section = $(this).parent().parent().next(".section");
+      section.slideToggle(300);
+      var text = $(this).text() == "show" ? "hide" : "show";
+      $(this).text(text);
+      return false;
+    });
+});
+
