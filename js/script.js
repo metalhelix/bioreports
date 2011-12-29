241d2aa465ade5fc5d2987be6579acb82679aabe
Jim V <jfv@stowers.org>
2011-12-29 13:52:27 -0600

    add comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $("#toc").tableOfContents(
     $("#main"),
     {
@@ -11,7 +12,6 @@ $(document).ready(function() {
     clearFiltersControls: [$('#cleanfilters')]
   };
 
-    
   $('.big_table').tableFilter(options);
   
   $('.data_table').dataTable( {
@@ -25,11 +25,18 @@ $(document).ready(function() {
     "bSort": true,
     "bInfo": false
   });
+
+  // wrap contents below h2's in div.section to 
+  // allow for show/hide functionality
   $('h2').each(function(index) {
     $(this).nextUntil('h2').wrapAll('<div class="section" />');
   });
+
+  // add show/hide toggle span to h2's
   $('#main h2').append("  <span class='header_accent'><a href='#' class='toggle'>hide</a></span>");
+  // add same show/hide toggle to .section_toggle 
   $('.section_toggle').append("  <span class='header_accent'><a href='#' class='toggle'>show</a></span>");
+  // add toggle functionality to a.toggle's
   $('a.toggle').click(function() 
     {
       var section = $(this).parent().parent().next(".section");
