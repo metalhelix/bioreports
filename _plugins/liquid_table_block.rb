e3f22912d33fe4b65a1df255f45aa45f173160d9
Jim V <jfv@stowers.org>
2012-01-06 09:04:54 -0600

    add class option to tableo table block

--- _plugins/liquid_table_block.rb
+++ _plugins/liquid_table_block.rb
@@ -7,16 +7,32 @@ module Jekyll
     DEFAULT_TYPE = "csv"
     VALID_TYPES = [DEFAULT_TYPE, "tsv"]
 
+    SYNTAX = /(\w+)\s?([\w\s=,]+)*/
     def initialize(tag_name, markup, tokens)
       super
-      @data_type = markup.strip
-      if !@data_type or @data_type.empty? or !VALID_TYPES.include?(@data_type)
-        @data_type = DEFAULT_TYPE
+      @options = {:class => "data_table display"}
+      @data_type = DEFAULT_TYPE
+      if markup =~ SYNTAX
+        @data_type = $1.strip
+        if defined? $2
+          $2.split(',').each do |opt|
+            key, value = opt.split('=')
+            if key and value
+              @options[key.strip.to_sym] = value.strip
+            else
+              puts "ERROR: invalid option for table"
+              puts $2
+            end
+          end
+        end
+      else
+        puts "ERROR: invalid input for table"
+        puts markup
       end
     end
 
     def render(context)
-      TableBuilder.render_table(super, @data_type)
+      TableBuilder.render_table(super, @data_type, @options)
     end
   end
 end
