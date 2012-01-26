de36d5984ca56e2bca768e0543e7f19d7bffae4f
Jim V <jfv@stowers.org>
2012-01-26 15:35:31 -0600

    use data meta data for image_link by default

--- _plugins/liquid_image_link.rb
+++ _plugins/liquid_image_link.rb
@@ -24,10 +24,41 @@ module Jekyll
     end
 
     def render(context)
-      page_id = context.registers[:page]["id"]
-      image_url = "data/#{page_id}/#{@image}".gsub("//","/")
+
+      image_url = nil
+
+      # first attempt to use the data field of the report
+      # meta data
+      data_dirs = context.registers[:page]["data"]
+      data_dirs ||= []
+
+      data_dirs.each do |data_dir|
+        image_url = File.join(data_dir, @image)
+        full_image_file = File.expand_path(image_url, context.registers[:site].source)
+        if File.exists? full_image_file
+          break
+        end
+        image_url = nil
+      end
+
+
+      # if that doesn't work, try to get the data dir by id
+      # NOTE: pages don't have an id - only posts
+      if !image_url
+        page_id = context.registers[:page]["id"]
+        image_url = "data/#{page_id}/#{@image}".gsub("//","/")
+        full_image_file = File.expand_path(image_url, context.registers[:site].source)
+        if !File.exists? full_image_file
+          image_url = nil
+        end
+      end
+
       image_class = @options[:class]
-      "!(#{image_class})#{image_url}!:#{image_url}"
+      if image_url
+        "!(#{image_class})#{image_url}!:#{image_url}"
+      else
+        "ERROR: cannot find #{@image} in #{data_dirs.join(", ")}"
+      end
     end
   end
 end
