c9d71e2c5173d6f38aa6ab2fa568e34bba94d150
Jim V <jfv@stowers.org>
2012-02-09 16:13:18 -0600

    make page changing not reload page

--- js/pages/data_grid.coffee
+++ js/pages/data_grid.coffee
@@ -9,7 +9,7 @@ data_grid.get_options = () ->
   options.sort = getURLParameter('sort') || null
   options
 
-data_grid.view = (id, data, options) ->
+data_grid.create_view = (id, data, options) ->
   grid = d3.select(id).append("table")
     .attr("id", "data_grid_table")
 
@@ -31,9 +31,14 @@ data_grid.view = (id, data, options) ->
         .style("width", "95%")
     filter_id_num += 1
 
-  grid_body = grid.append("tbody")
+  data_grid.grid_body = grid.append("tbody")
 
-  grid_rows = grid_body.selectAll("tr")
+data_grid.refresh_view = (data, options) ->
+  header_data = data_grid.header(data)
+
+  grid_rows = data_grid.grid_body.selectAll("tr").remove()
+
+  grid_rows = data_grid.grid_body.selectAll("tr")
     .data(data)
 
   grid_row = grid_rows.enter()
@@ -43,27 +48,33 @@ data_grid.view = (id, data, options) ->
     grid_row.append("td")
       .text((d) -> d[header])
 
-data_grid.view_pagination = (id, data, options) ->
+data_grid.create_view_pagination = (id, data, options) ->
   min_pages = 1
   max_pages = Math.ceil(data.length / options.limit)
   current_page = options.page
   pagination = d3.select(id).attr("class", "data_grid_page_links")
 
-  pagination.selectAll("a")
-    .data([min_pages..max_pages]).enter()
-  .append("a")
+  pages = pagination.selectAll("a").remove()
+
+  pages = pagination.selectAll("a")
+    .data([min_pages..max_pages])
+
+  pages.enter().append("a")
     .attr("id", (d) -> "data_grid_page_link_#{d}")
     .attr("data-page", (d) -> d)
     .attr("href", "#")
     .attr("class", (d) -> if d == current_page then "data_grid_page_link current" else "data_grid_page_link")
     .text((d) -> "#{d}")
-    .on("click", (d) -> insertParam("page", d))
+    .on "click", (d) ->
+      options.page = d
+      data_grid.refresh(data, options)
+
+  pages.exit().remove()
 
 
 data_grid.sort = (data, options) ->
   return if !options.sort
-  data.sort (a,b) ->
-    console.log(a[2])
+  data
 
 data_grid.limit = (data, options) ->
   max_pages = Math.ceil(data.length / options.limit)
@@ -71,7 +82,6 @@ data_grid.limit = (data, options) ->
   current_page = Math.min(current_page, max_pages)
   limit_start = (current_page - 1) * options.limit
   limit_end = Math.min((current_page) * options.limit, data.length) - 1
-  console.log("#{limit_start} - #{limit_end}")
   data[limit_start..limit_end]
 
 data_grid.header = (data) ->
@@ -80,23 +90,31 @@ data_grid.header = (data) ->
 data_grid.show = (data, options) ->
   d3.select("#data_grid").html("<div id=\"data_grid_pagination_top\"></div><div id=\"data_grid_data\"></div><div id=\"data_grid_pagination_bottom\"></div>")
 
-  data_grid.view_pagination("#data_grid_pagination_top", data, options)
-  data_grid.view_pagination("#data_grid_pagination_bottom", data, options)
+  data_grid.create_view("#data_grid_data", data, options)
+  data_grid.refresh(data,  options)
+
+data_grid.refresh = (data, options) ->
+  console.log("refresh")
+  console.log(options)
   data_grid.sort(data,options)
-  page_data = data_grid.limit(data,options)
-  data_grid.view("#data_grid_data", page_data, options)
+  data_grid.create_view_pagination("#data_grid_pagination_top", data, options)
+  data_grid.create_view_pagination("#data_grid_pagination_bottom", data, options)
+  page_data = data_grid.limit(data, options)
+  data_grid.refresh_view(page_data, options)
+
 
 $ ->
   options = data_grid.get_options()
 
-  show_grid = (data) ->
+  data_loaded = (data) ->
+    console.log("loaded")
     data_grid.show(data,options)
 
   error_no_file = () ->
     d3.select("#data_grid").html("<h2 class=\"error\">ERROR: no file provided</h2>")
 
   if options.filename
-    d3.csv options.filename, show_grid
+    d3.csv options.filename, data_loaded
   else
     error_no_file()
 
--- js/pages/data_grid.js
+++ js/pages/data_grid.js
@@ -10,8 +10,8 @@
     options.sort = getURLParameter('sort') || null;
     return options;
   };
-  data_grid.view = function(id, data, options) {
-    var filter_id_num, grid, grid_body, grid_filters, grid_header, grid_row, grid_rows, grid_titles, header, header_data, _i, _j, _k, _len, _len2, _len3, _results;
+  data_grid.create_view = function(id, data, options) {
+    var filter_id_num, grid, grid_filters, grid_header, grid_titles, header, header_data, _i, _j, _len, _len2;
     grid = d3.select(id).append("table").attr("id", "data_grid_table");
     header_data = data_grid.header(data);
     grid_header = grid.append("thead");
@@ -27,29 +27,36 @@
       grid_filters.append("td").append("input").attr("id", "filter_" + filter_id_num).attr("type", "text").style("width", "95%");
       filter_id_num += 1;
     }
-    grid_body = grid.append("tbody");
-    grid_rows = grid_body.selectAll("tr").data(data);
+    return data_grid.grid_body = grid.append("tbody");
+  };
+  data_grid.refresh_view = function(data, options) {
+    var grid_row, grid_rows, header, header_data, _i, _len, _results;
+    header_data = data_grid.header(data);
+    grid_rows = data_grid.grid_body.selectAll("tr").remove();
+    grid_rows = data_grid.grid_body.selectAll("tr").data(data);
     grid_row = grid_rows.enter().append("tr");
     _results = [];
-    for (_k = 0, _len3 = header_data.length; _k < _len3; _k++) {
-      header = header_data[_k];
+    for (_i = 0, _len = header_data.length; _i < _len; _i++) {
+      header = header_data[_i];
       _results.push(grid_row.append("td").text(function(d) {
         return d[header];
       }));
     }
     return _results;
   };
-  data_grid.view_pagination = function(id, data, options) {
-    var current_page, max_pages, min_pages, pagination, _i, _results;
+  data_grid.create_view_pagination = function(id, data, options) {
+    var current_page, max_pages, min_pages, pages, pagination, _i, _results;
     min_pages = 1;
     max_pages = Math.ceil(data.length / options.limit);
     current_page = options.page;
     pagination = d3.select(id).attr("class", "data_grid_page_links");
-    return pagination.selectAll("a").data((function() {
+    pages = pagination.selectAll("a").remove();
+    pages = pagination.selectAll("a").data((function() {
       _results = [];
       for (var _i = min_pages; min_pages <= max_pages ? _i <= max_pages : _i >= max_pages; min_pages <= max_pages ? _i++ : _i--){ _results.push(_i); }
       return _results;
-    }).apply(this)).enter().append("a").attr("id", function(d) {
+    }).apply(this));
+    pages.enter().append("a").attr("id", function(d) {
       return "data_grid_page_link_" + d;
     }).attr("data-page", function(d) {
       return d;
@@ -62,16 +69,16 @@
     }).text(function(d) {
       return "" + d;
     }).on("click", function(d) {
-      return insertParam("page", d);
+      options.page = d;
+      return data_grid.refresh(data, options);
     });
+    return pages.exit().remove();
   };
   data_grid.sort = function(data, options) {
     if (!options.sort) {
       return;
     }
-    return data.sort(function(a, b) {
-      return console.log(a[2]);
-    });
+    return data;
   };
   data_grid.limit = function(data, options) {
     var current_page, limit_end, limit_start, max_pages;
@@ -80,32 +87,38 @@
     current_page = Math.min(current_page, max_pages);
     limit_start = (current_page - 1) * options.limit;
     limit_end = Math.min(current_page * options.limit, data.length) - 1;
-    console.log("" + limit_start + " - " + limit_end);
     return data.slice(limit_start, (limit_end + 1) || 9e9);
   };
   data_grid.header = function(data) {
     return d3.keys(data[0]);
   };
   data_grid.show = function(data, options) {
-    var page_data;
     d3.select("#data_grid").html("<div id=\"data_grid_pagination_top\"></div><div id=\"data_grid_data\"></div><div id=\"data_grid_pagination_bottom\"></div>");
-    data_grid.view_pagination("#data_grid_pagination_top", data, options);
-    data_grid.view_pagination("#data_grid_pagination_bottom", data, options);
+    data_grid.create_view("#data_grid_data", data, options);
+    return data_grid.refresh(data, options);
+  };
+  data_grid.refresh = function(data, options) {
+    var page_data;
+    console.log("refresh");
+    console.log(options);
     data_grid.sort(data, options);
+    data_grid.create_view_pagination("#data_grid_pagination_top", data, options);
+    data_grid.create_view_pagination("#data_grid_pagination_bottom", data, options);
     page_data = data_grid.limit(data, options);
-    return data_grid.view("#data_grid_data", page_data, options);
+    return data_grid.refresh_view(page_data, options);
   };
   $(function() {
-    var error_no_file, options, show_grid;
+    var data_loaded, error_no_file, options;
     options = data_grid.get_options();
-    show_grid = function(data) {
+    data_loaded = function(data) {
+      console.log("loaded");
       return data_grid.show(data, options);
     };
     error_no_file = function() {
       return d3.select("#data_grid").html("<h2 class=\"error\">ERROR: no file provided</h2>");
     };
     if (options.filename) {
-      return d3.csv(options.filename, show_grid);
+      return d3.csv(options.filename, data_loaded);
     } else {
       return error_no_file();
     }
