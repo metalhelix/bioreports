bdb695b2e84e9ac5d35fb3a017b39c9520c15629
Jim V <jfv@stowers.org>
2012-04-27 10:10:36 -0500

    hide extra links by default

---
+++ js/pages/dashboard.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $("#researchers_link").click(function() {
+    $("#researchers").toggle("fast");
+    return false;
+  });
+  $("#analysts_link").click(function() {
+    $("#analysts").toggle("fast");
+    return false;
+  });
+  $("#pis_link").click(function() {
+    $("#pis").toggle("fast");
+    return false;
+  });
+  $("#categories_link").click(function() {
+    $("#categories").toggle("fast");
+    return false;
+  });
+
+});
